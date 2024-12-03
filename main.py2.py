98ef2d5e27fb6f22179f481b3569f516e5ea95c0
dmytro <pokerface4455@gmail.com>
2024-12-03 15:47:07 +0100

    second commit

--- main.py2.py
+++ main.py2.py
@@ -1,1 +1,9 @@
-jjjj
+user_number = input("Введіть 5-ти значне число: ")
+number = int(user_number)
+a = number % 10
+b = (number // 10) % 10
+c = (number // 100) % 10
+d = (number // 1000) % 10
+e = (number // 10000)
+print(str(a) + str(b) + str(c) + str(d) + str(e))
+
